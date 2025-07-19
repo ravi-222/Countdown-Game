dcdf48618a916a91ab50614524eadc02f633ab9a
ravi-222 <ravindersinghanand22@gmail.com>
2025-07-18 23:18:41 -0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import Player from "./components/Player.jsx";
+import TimerChallenge from "./components/TimerChallenge.jsx";
+
+function App() {
+  return (
+    <>
+      <Player />
+      <div id="challenges"></div>
+      <div className="challenges">
+        <TimerChallenge title="Easy" targetTime={1} />
+        <TimerChallenge title="Medium" targetTime={5} />
+        <TimerChallenge title="Hard" targetTime={10} />
+      </div>
+    </>
+  );
+}
+
+export default App;
